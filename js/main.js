bda71cc5cd9672a0af60c9ad4cb08e8c775fdd19
drainpip <drainpip@gmail.com>
2013-01-24 23:57:01 -0800

    Hacked it
    Wasn't building URL properly so I built it myself.

--- js/main.js
+++ js/main.js
@@ -10,4 +10,133 @@
  * Copyright (c) 2012 Giovanni Cappellotto
  * Licensed MIT
  */
-(function(e){e.fn.instagram=function(t){function s(t){var n=t.images.thumbnail.url;if(i.image_size=="low_resolution"){n=t.images.low_resolution.url}else if(i.image_size=="thumbnail"){n=t.images.thumbnail.url}else if(i.image_size=="standard_resolution"){n=t.images.standard_resolution.url}var r=e("<img>").addClass("instagram-image").attr("src",n);if(i.photoLink){r=e("<a>").attr("target","_blank").attr("href",t.link).append(r)}return e("<div>").addClass("instagram-placeholder").attr("id",t.id).append(r)}function o(){return e("<div>").addClass("instagram-placeholder").attr("id","empty").append(e("<p>").html("No photos for this query"))}function u(){var t=r,n={};if(i.next_url!=null){return i.next_url}if(i.hash!=null){t+="/tags/"+i.hash+"/media/recent"}else if(i.search!=null){t+="/media/search";n.lat=i.search.lat;n.lng=i.search.lng;i.search.max_timestamp!=null&&(n.max_timestamp=i.search.max_timestamp);i.search.min_timestamp!=null&&(n.min_timestamp=i.search.min_timestamp);i.search.distance!=null&&(n.distance=i.search.distance)}else if(i.userId!=null){t+="/users/"+i.userId+"/media/recent"}else if(i.locationId!=null){t+="/locations/"+i.locationId+"/media/recent"}else{t+="/media/popular"}i.accessToken!=null&&(n.access_token=i.accessToken);i.clientId!=null&&(n.client_id=i.clientId);i.minId!=null&&(n.min_id=i.minId);i.maxId!=null&&(n.max_id=i.maxId);i.show!=null&&(n.count=i.show);t+="?"+e.param(n);return t}var n=this,r="https://api.instagram.com/v1",i={hash:null,userId:null,locationId:null,search:null,accessToken:null,clientId:null,show:null,onLoad:null,onComplete:null,maxId:null,minId:null,next_url:null,image_size:null,photoLink:true};t&&e.extend(i,t);i.onLoad!=null&&typeof i.onLoad=="function"&&i.onLoad();e.ajax({type:"GET",dataType:"jsonp",cache:false,url:u(),success:function(e){var t=typeof e.data!="undefined"?e.data.length:0;var r=i.show!=null&&i.show<t?i.show:t;if(r>0){for(var u=0;u<r;u++){n.append(s(e.data[u]))}}else{n.append(o())}i.onComplete!=null&&typeof i.onComplete=="function"&&i.onComplete(e.data,e)}});return this}})(jQuery);
+
+(function ($){
+  $.fn.instagram = function (options) {
+    var that = this,
+        apiEndpoint = "https://api.instagram.com/v1",
+        settings = {
+            hash: null
+          , userId: null
+          , locationId: null
+          , search: null
+          , accessToken: null
+          , clientId: null
+          , show: null
+          , onLoad: null
+          , onComplete: null
+          , maxId: null
+          , minId: null
+          , next_url: null
+          , image_size: null
+          , photoLink: true
+        };
+        
+    options && $.extend(settings, options);
+    
+    function createPhotoElement(photo) {
+      var image_url = photo.images.thumbnail.url;
+      
+      if (settings.image_size == 'low_resolution') {
+        image_url = photo.images.low_resolution.url;
+      }
+      else if (settings.image_size == 'thumbnail') {
+        image_url = photo.images.thumbnail.url;
+      }
+      else if (settings.image_size == 'standard_resolution') {
+        image_url = photo.images.standard_resolution.url;
+      }
+
+      var innerHtml = $('<img>')
+        .addClass('instagram-image')
+        .attr('src', image_url);
+
+      if (settings.photoLink) {
+        innerHtml = $('<a>')
+          .attr('target', '_blank')
+          .attr('href', photo.link)
+          .append(innerHtml);
+      }
+
+      return $('<div>')
+        .addClass('instagram-placeholder')
+        .attr('id', photo.id)
+        .append(innerHtml);
+    }
+    
+    function createEmptyElement() {
+      return $('<div>')
+        .addClass('instagram-placeholder')
+        .attr('id', 'empty')
+        .append($('<p>').html('No photos for this query'));
+    }
+    
+    function composeRequestURL() {
+
+      var url = apiEndpoint,
+          params = {};
+      
+      if (settings.next_url != null) {
+        return settings.next_url;
+      }
+
+      if (settings.hash != null) {
+        url += "/tags/" + settings.hash + "/media/recent";
+      }
+      else if (settings.search != null) {
+        url += "/media/search";
+        params.lat = settings.search.lat;
+        params.lng = settings.search.lng;
+        settings.search.max_timestamp != null && (params.max_timestamp = settings.search.max_timestamp);
+        settings.search.min_timestamp != null && (params.min_timestamp = settings.search.min_timestamp);
+        settings.search.distance != null && (params.distance = settings.search.distance);
+      }
+      else if (settings.userId != null) {
+        url += "/users/" + settings.userId + "/media/recent";
+      }
+      else if (settings.locationId != null) {
+        url += "/locations/" + settings.locationId + "/media/recent";
+      }
+      else {
+        url += "/media/popular";
+      }
+      
+      settings.accessToken != null && (params.access_token = settings.accessToken);
+      settings.clientId != null && (params.client_id = settings.clientId);
+      settings.minId != null && (params.min_id = settings.minId);
+      settings.maxId != null && (params.max_id = settings.maxId);
+      settings.show != null && (params.count = settings.show);
+
+      url += "?" + $.param(params)
+      
+	  url = "https://api.instagram.com/v1/users/33278947/media/recent?access_token=33278947.5eb6e33.73c5d6a93eb94beca7c34b41a2b1a545"
+      return url;
+    }
+    
+    settings.onLoad != null && typeof settings.onLoad == 'function' && settings.onLoad();
+    
+    $.ajax({
+      type: "GET",
+      dataType: "jsonp",
+      cache: false,
+      url: composeRequestURL(),
+      success: function (res) {
+        var length = typeof res.data != 'undefined' ? res.data.length : 0;
+        var limit = settings.show != null && settings.show < length ? settings.show : length;
+        
+        if (limit > 0) {
+          for (var i = 0; i < limit; i++) {
+            that.append(createPhotoElement(res.data[i]));
+          }
+        }
+        else {
+          that.append(createEmptyElement());
+        }
+
+        settings.onComplete != null && typeof settings.onComplete == 'function' && settings.onComplete(res.data, res);
+      }
+    });
+    
+    return this;
+  };
+})(jQuery);
