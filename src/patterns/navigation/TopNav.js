3d8bd632b0426bacf7b2170969fbb9c92d3abe70
drainpip <me@shaneis.me>
2017-04-17 15:34:58 -0700

    Set up NavLinks

--- src/patterns/navigation/TopNav.js
+++ src/patterns/navigation/TopNav.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './TopNav.css';
 
 const TopNav = (props) => {
   return (
     <div>
-      <h1 className="App-header__title has-navigation">Shane is me</h1>
-      <p>Our navigation goes here</p>
+      <h1 className="App-header__title has-navigation"><Link to="/">Shane is me</Link></h1>
+      <ul className="TopNav">
+        <li><NavLink to="/experiments" className="TopNav__Link" activeClassName="is-active">Experiments</NavLink></li>
+        <li><NavLink to="/organization" className="TopNav__Link" activeClassName="is-active">Organization</NavLink></li>
+      </ul>
     </div>
   );
 };
