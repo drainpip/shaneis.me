ef27492860e8f5eb05c854c1d4ad5dd79f7a7f8a
drainpip <me@shaneis.me>
2018-04-20 12:39:14 -0700

    Delete unused files

--- src/patterns/containers/Content.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Content.css';
-
-const ContentContainer = props => {
-  return <div className="Content-Container">{props.children}</div>;
-};
-
-export default ContentContainer;
